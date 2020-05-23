33f108f318213401563f0605c61897666bd83f4b
koushik1 <kshanka2@ncsu.edu>
2020-05-23 17:11:28 -0400

    Added a Search bar

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class SearchBar extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {search_input: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e)
+  {
+    this.setState({search_input: e.target.value})
+  }
+
+  handleSubmit(e) {
+    alert('A search was submitted: ' + this.state.search_input);
+    e.preventDefault();
+  }
+
+  render()
+  {
+    return(
+        <div>
+          <h1>
+            Wiki Viewer
+          </h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <input type = "text" 
+            placeholder = "Search Wikipedia" 
+            value = {this.state.search_input}
+            onChange = {this.handleChange}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+    )
+
+  }
+
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SearchBar />
+  )
+
 }
 
 export default App;
